92ad7c2fd80edb657c15faa26f83298c6b83c302
Rashid Saleem <rsaleem@stellatechnology.com>
2020-08-17 22:00:59 +0500

    Add requirements.txt file

--- forumApp/settings.py
+++ forumApp/settings.py
@@ -11,20 +11,29 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config, Csv
+import dj_database_url
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# from decouple import config
+
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l$3^%!)$u**d5s(9wqk)$9(4$4^1uxh4l7&1=+!@_^b#j9e^%g'
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'l$3^%!)$u**d5s(9wqk)$9(4$4^1uxh4l7&1=+!@_^b#j9e^%g'
+# environment variables
+SECRET_KEY = config('SECRET_KEY')
+
+# # or local files
+# with open('/etc/secret_key.txt') as f:
+#     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
 
